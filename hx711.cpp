4068fdd0afa4e3657de290703fc0c90fbfb17b4f
Aleksi Mattila <aleksi@irtimaasta.fi>
2021-03-05 18:18:34 +0200

    testing changes

--- hx711.cpp
+++ hx711.cpp
@@ -1,133 +1,181 @@
 #include <stdio.h>
 #include <inttypes.h>
+#ifdef __arm__
 #include <wiringPi.h>
-
+#include <unistd.h>
+#endif
 #include "hx711.h"
 
-HX711::HX711(uint8_t clockPin, uint8_t dataPin, uint8_t skipSetup) :
-	mGainBits(1),
-	mScale(1.0f),
-	mOffset(0),
-	mClockPin(clockPin),
-	mDataPin(dataPin)
+#ifdef __arm__
+HX711* hxp = NULL;
+
+void readInstance() {
+  hxp->mDataReady = true;
+  hxp->read();
+}
+#endif
+
+HX711::HX711(uint8_t clockPin, uint8_t dataPin) :
+  mGainBits(1),
+  mScale(1.0f),
+  mOffset(0),
+  mClockPin(clockPin),
+  mDataPin(dataPin)
 {
-	this->initialize(skipSetup);
+#ifdef __arm__
+  wiringPiSetupGpio();
+
+  pinMode(mClockPin, OUTPUT);
+  pinMode(mDataPin, INPUT);
+
+  hxp = this;
+  // Setup interrupt on falling edge of data pin.
+  wiringPiISR(mDataPin, INT_EDGE_FALLING, readInstance);
+  this->mReading = false;
+  this->mDataReady = false;
+#endif
 }
 
-void HX711::initialize(uint8_t skipSetup){
-	if((!skipSetup) && wiringPiSetup() == -1){
-		printf("initialization failed");
-	}
-	pinMode(mClockPin, OUTPUT);
-	pinMode(mDataPin, INPUT);
+void HX711::setGain(uint8_t gain)
+{
+  switch (gain)
+  {
+  // channel A, gain 128
+  case 128:
+    this->mGainBits = 1;
+    break;
+  // channel A, gain 64
+  case 64:
+    this->mGainBits = 3;
+    break;
+  // channel B, gain 32
+  case 32:
+    this->mGainBits = 2;
+    break;
+  }
+
+#ifdef __arm__
+  digitalWrite(mClockPin, LOW);
+  read();
+#endif
 }
 
-bool HX711::isReady(){
-	return digitalRead(mDataPin) == LOW;
+void HX711::read() {
+#ifdef __arm__
+  if (!this->mReading && this->mDataReady) {
+    this->mReading = true;
+
+    int32_t data = 0;
+    // pulse the clock pin 24 times to read the data
+    for (uint8_t i = 24; i--;)
+    {
+      digitalWrite(mClockPin, HIGH);
+      delayMicroseconds(1);
+      digitalWrite(mClockPin, LOW);
+
+      data |= (digitalRead(mDataPin) << i);
+    }
+
+    // set the channel and the gain factor for the next reading
+    for (int i = 0; i < mGainBits; i++)
+    {
+      digitalWrite(mClockPin, HIGH);
+      delayMicroseconds(1);
+      digitalWrite(mClockPin, LOW);
+    }
+
+    if (data & 0x800000)
+    {
+      data |= 0xff000000UL;
+    }
+
+    if(this->mTimes != 0 && data != -1) {
+      this->mLatestData = data;
+      this->mSum += this->mLatestData;
+      this->mTimes--;
+    }
+    
+    this->mDataReady = false;
+    this->mReading = false;
+  }
+
+#else
+  this->mLatestData = 0;
+#endif
 }
 
-void HX711::setGain(uint8_t gain){
-	switch(gain){
-		case GAIN_128:
-			this->mGainBits = 1;
-		break;
-		case GAIN_64:
-			this->mGainBits = 3;
-		break;
-		case GAIN_32:
-			this->mGainBits = 2;
-		break;
-		default:
-			//invalid gain, ignore
-		break;
-	}
+int32_t HX711::readAverage()
+{
+#ifdef __arm__
+  this->mSum = 0;
+  int8_t times = this->mTimes;
 
-	digitalWrite(mClockPin, LOW);
-	read();
+  // TODO: find the sweetspot for best CPU usage and sensor resonse time.
+  // Increasing the sleep time reduces CPU usage yet it might reduce the read speed too.
+  // Fİgure out the sensor response time and set it accordingly.
+  while(this->mTimes >= 1) usleep(100);
+
+  return this->mSum / times;
+#else
+  return 0;
+#endif
 }
 
-int32_t HX711::read() {
-	// wait for the chip to become ready
-	while (!this->isReady());
-
-	int32_t data = 0;
-	// pulse the clock pin 24 times to read the data
-	for(uint8_t i = 24; i--;){
-		digitalWrite(mClockPin, HIGH);
-
-		digitalRead(mDataPin);
-		data |= (digitalRead(mDataPin) << i);
-
-		digitalWrite(mClockPin, LOW);
-	}
-
-	// set the channel and the gain factor for the next reading using the clock pin
-	for (int i = 0; i < mGainBits; i++) {
-		digitalWrite(mClockPin, HIGH);
-		digitalWrite(mClockPin, LOW);
-	}
-
-	if(data & 0x800000){
-//		data |= (long) ~0xffffff;
-		data |= 0xFF000000UL;
-	}
-
-	return data;
+int32_t HX711::getRawValue()
+{
+  return readAverage() - mOffset;
 }
 
-int32_t HX711::readAverage(uint8_t times) {
-	int64_t sum = 0;
-	for (uint8_t i = 0; i < times; i++) {
-		sum += read();
-	}
-	return sum / times;
+float HX711::getUnits(uint8_t times)
+{
+  this->mTimes = times;
+  return getRawValue() / mScale;
 }
 
-int32_t HX711::getRawValue(uint8_t times) {
-	return readAverage(times) - mOffset;
+void HX711::tare(uint8_t times)
+{
+  this->mTimes = times;
+  uint64_t sum = readAverage();
+  setOffset(sum);
 }
 
-float HX711::getUnits(uint8_t times) {
-	return getRawValue(times) / mScale;
+void HX711::setScale(float scale)
+{
+  this->mScale = scale;
 }
 
-void HX711::tare(uint8_t times) {
-	uint64_t sum = readAverage(times);
-	setOffset(sum);
+void HX711::setOffset(int32_t offset)
+{
+  this->mOffset = offset;
 }
 
-void HX711::setScale(float scale) {
-	mScale = scale;
+void HX711::powerDown()
+{
+#ifdef __arm__
+  digitalWrite(mClockPin, LOW);
+  digitalWrite(mClockPin, HIGH);
+  // The sensor powers down after 60 useconds
+  usleep(60);
+#endif
 }
 
-void HX711::setOffset(int32_t offset) {
-	mOffset = offset;
+void HX711::powerUp()
+{
+#ifdef __arm__
+  digitalWrite(mClockPin, LOW);
+#endif
 }
 
-void HX711::powerDown() {
-	digitalWrite(mClockPin, LOW);
-	digitalWrite(mClockPin, HIGH);
+int32_t HX711::getOffset()
+{
+  return this->mOffset;
 }
 
-void HX711::powerUp() {
-	digitalWrite(mClockPin, LOW);
+float HX711::getScale()
+{
+  return this->mScale;
 }
 
-int32_t HX711::getOffset(){
-	return this->mOffset;
+int32_t HX711::getLatestData() {
+  return this->mLatestData;
 }
-
-float HX711::getScale(){
-	return this->mScale;
-}
-
-int main(){
-	HX711 sensor(7, 0, 0);
-	sensor.tare();
-	sensor.setScale(16000);
-	while(true){
-		printf("%f\n", sensor.getUnits());
-		printf("%d\n", sensor.readAverage());
-	}
-}
-
--- hx711.h
+++ hx711.h
@@ -1,42 +1,37 @@
+#pragma once
+
 #include <inttypes.h>
 
-#ifndef _HX711_H_
-#define _HX711_H_
+class HX711
+{
 
-//channel A, gain 128
-#define GAIN_128 128
-
-//channel A, gain 64
-#define GAIN_64 64
-
-//channel B, gain 32
-#define GAIN_32 32
-
-class HX711{
 private:
-	uint8_t mGainBits;
-	float mScale;
-	int32_t mOffset;
+  uint8_t mGainBits;
+  float mScale;
+  int32_t mOffset;
+  int32_t mLatestData;
+  int64_t mSum;
 
-	uint8_t mClockPin;
-	uint8_t mDataPin;
+  uint8_t mClockPin;
+  uint8_t mDataPin;
+  uint8_t mTimes;
+  bool mReading;
 
 public:
-	HX711(uint8_t clockPin, uint8_t dataPin, uint8_t skipSetup);
-	void initialize(uint8_t skipSetup);
-	bool isReady();
-	void setGain(uint8_t gain);
-	int32_t read();
-	int32_t readAverage(uint8_t times = 10);
-	int32_t getRawValue(uint8_t times = 10);
-	float getUnits(uint8_t times = 10);
-	void tare(uint8_t times = 10);
-	void setScale(float scale);
-	void setOffset(int32_t offset);
-	void powerDown();
-	void powerUp();
-	int32_t getOffset();
-	float getScale();
-};
+  bool mDataReady;
+  HX711(uint8_t clockPin, uint8_t dataPin);
+  void setGain(uint8_t gain);
+  void read();
+  int32_t readAverage();
+  int32_t getRawValue();
+  float getUnits(uint8_t times = 10);
+  void tare(uint8_t times = 10);
+  void setScale(float scale);
+  void setOffset(int32_t offset);
+  void powerDown();
+  void powerUp();
+  int32_t getOffset();
+  float getScale();
+  int32_t getLatestData();
 
-#endif //_HX711_H_
+};
